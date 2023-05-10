bff3d8945a42948043862660d74fda042027f71e
Andy Kruch <avk.16@yandex.ru>
2023-05-10 20:39:08 +0300

    Constraints add

---
+++ ImageFeedAVK/ImagesList/ImagesListCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImagesListCell.swift
+//  ImageFeedAVK
+//
+//  Created by Andy Kruch on 08.05.23.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
