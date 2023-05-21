cd8bfd7a92b8e036a5e8dda637e64a78e7ed6bae
Andy Kruch <avk.16@yandex.ru>
2023-05-21 21:09:36 +0300

    Single Image View Add

---
+++ ImageFeedAVK/ImagesList/SingleImageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeedAVK
+//
+//  Created by Andy Kruch on 21.05.23.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+}
