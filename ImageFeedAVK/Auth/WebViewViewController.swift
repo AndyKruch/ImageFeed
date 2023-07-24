fde452fc79fd29957fbf6de5457dc8bee6e5fa2f
Andy Kruch <avk.16@yandex.ru>
2023-07-24 11:26:34 +0300

    Fixes 2

--- ImageFeedAVK/Auth/WebViewViewController.swift
+++ ImageFeedAVK/Auth/WebViewViewController.swift
@@ -8,13 +8,12 @@
 import UIKit
 import WebKit
 
-
 protocol WebViewViewControllerDelegate: AnyObject {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String)
     func webViewViewControllerDidCancel(_ vc: WebViewViewController)
 }
 
-public protocol WebViewViewControllerProtocol: AnyObject {
+protocol WebViewViewControllerProtocol: AnyObject {
     var presenter: WebViewPresenterProtocol? { get set }
     func load(request: URLRequest)
     func setProgressValue(_ newValue: Float)
@@ -22,58 +21,54 @@ public protocol WebViewViewControllerProtocol: AnyObject {
 }
 
 final class WebViewViewController: UIViewController & WebViewViewControllerProtocol {
-    var presenter: WebViewPresenterProtocol?
-
-    @IBOutlet private var webView: WKWebView!
-    @IBOutlet private var progressView: UIProgressView!
-
+    @IBOutlet private weak var webView: WKWebView!
+    @IBOutlet private weak var progressView: UIProgressView!
+    private var estimatedProgressObservation: NSKeyValueObservation?
     weak var delegate: WebViewViewControllerDelegate?
-
+    var presenter: WebViewPresenterProtocol?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         webView.navigationDelegate = self
         presenter?.viewDidLoad()
+        estimatedProgressObservation = webView.observe(
+                     \.estimatedProgress,
+                      options: [],
+                      changeHandler: { [weak self] _, _ in
+                          guard let self = self else { return }
+                          self.presenter?.didUpdateProgressValue(webView.estimatedProgress)
+                      })
     }
-
-    @IBAction private func didTapBackButton(_ sender: Any?) {
-        delegate?.webViewViewControllerDidCancel(self)
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        // NOTE: Since the class is marked as `final` we don't need to pass a context.
-        // In case of inhertiance context must not be nil.
-        webView.addObserver(
-            self,
-            forKeyPath: #keyPath(WKWebView.estimatedProgress),
-            options: .new,
-            context: nil)
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), context: nil)
-    }
-
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if keyPath == #keyPath(WKWebView.estimatedProgress) {
-            presenter?.didUpdateProgressValue(webView.estimatedProgress)
-        } else {
-            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
-        }
-    }
-
-    func load(request: URLRequest) {
-        webView.load(request)
-    }
-
+    
     func setProgressValue(_ newValue: Float) {
         progressView.progress = newValue
     }
-
+    
     func setProgressHidden(_ isHidden: Bool) {
         progressView.isHidden = isHidden
+    }
+    
+    func load(request: URLRequest) {
+        webView.load(request)
+    }
+    
+    @IBAction private func didTapBackButton(_ sender: Any) {
+        delegate?.webViewViewControllerDidCancel(self)
+    }
+}
+
+// MARK: - Extensions
+
+extension WebViewViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView,
+                 decidePolicyFor navigationAction: WKNavigationAction,
+                 decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if let code = fetchCode(from: navigationAction) {
+            delegate?.webViewViewController(self, didAuthenticateWithCode: code)
+            decisionHandler(.cancel)
+        } else {
+            decisionHandler(.allow)
+        }
     }
     
     static func clean() {
@@ -84,37 +79,12 @@ final class WebViewViewController: UIViewController & WebViewViewControllerProto
             }
         }
     }
-    
-    private func configureBackBarButton() {
-        let backButtonView = UIView(frame: CGRect(x: 0, y: 0, width: 64, height: 44))
-        let imageView = UIImageView(image: .ypBackBarButtonImage)
-        imageView.frame = CGRect(x: 12, y: 16, width: 8.97, height: 15.59)
-        imageView.image = imageView.image!.withRenderingMode(.alwaysTemplate)
-        imageView.tintColor = .ypBlack
-        backButtonView.addSubview(imageView)
-        backButtonView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapBackButton)))
-        let barButton = UIBarButtonItem(customView: backButtonView)
-        navigationItem.leftBarButtonItem = barButton
-    }
 }
 
-extension WebViewViewController: WKNavigationDelegate {
-    func webView(
-        _ webView: WKWebView,
-        decidePolicyFor navigationAction: WKNavigationAction,
-        decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
-    ) {
-        if let code = code(from: navigationAction) {
-            delegate?.webViewViewController(self, didAuthenticateWithCode: code)
-            decisionHandler(.cancel)
-        } else {
-            decisionHandler(.allow)
-        }
-    }
-
-    private func code(from navigationAction: WKNavigationAction) -> String? {
+private extension WebViewViewController {
+    func fetchCode(from navigationAction: WKNavigationAction) -> String? {
         if let url = navigationAction.request.url {
-            return presenter?.code(from: url)
+            return presenter?.fetchCode(from: url)
         }
         return nil
     }
